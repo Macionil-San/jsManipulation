1a4af3d6aeef4c38a9e32bc9439cabd958648cbe
ruan macionil <macionilruan@gmail.com>
2024-03-28 21:49:10 -0300

    atualizando arquivos

--- jsmanipulation/script .js
+++ jsmanipulation/script .js
@@ -1,18 +1,67 @@
-document.addEventListener('DOMContentLoaded', () => {
-    alert("A página foi totalmente carregada e js foi implantado")
+//adicionando o evento de carregamento do documento (html)
+document.addEventListener('DOMContentLoaded', (event) => {
 
-    loadUserData()
+    event.preventDefault();
+    //evento para chamar o alert
+    // alert("A página foi totalmente carregada e js foi implantado!")
+
+    const form = document.querySelector('form')          // tem esses dois jeitos de fazer
+    form.addEventListener('submit', loadUserData);
+
+    loadUserDataList();
+    //chamando a função para carregar os usuários
 })
 
+// aray - vetores
 
-function loadUserDat (event) {
+function loadUserData(event) {
+    event.preventDefault();
 
-    event.presentDefaul()
+    let listUser = []
 
-    const userData= {
-
+    //capturando os valores e colocando eles dentro de um objeto (userData)
+    const userData = {
+        //capturando os valores e colocando eles dentro das propriedades ou também atributos
         name: document.getElementById('name').value,
         email: document.getElementById('email').value,
         age: document.getElementById('age').value
     }
+
+    if (localStorage.getItem('users')) {
+        listUser = JSON.parse(localStorage.getItem('users'))
+    }
+
+    listUser.push(userData)
+    localStorage.setItem('users', JSON.stringify(listUser))
+
+    console.log(listUser)
+    window.location.reload();
+
+}
+
+const loadUserDataList = () => {
+
+    const tableData = document.getElementById('tableBodyList');
+
+    if (localStorage.getItem('users')) {
+        const lisUser = JSON.parse(localStorage.getItem('users'))  // jason para objeto
+
+        let templete = ""
+
+        lisUser.forEach(user => {
+            templete += ` 
+                <tr>
+                    <td> ${user.name} </td> 
+        '           <td> ${user.email} </td> 
+                    <td> ${user.age} </td> 
+                </tr>
+    `
+        }); 
+        
+        tableData.innerHTML = templete
+    } else {
+        ("nenhum usuário cadatrado")
+
+    }
+
 }
